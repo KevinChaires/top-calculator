36373479eab7738ec3e52c89b0e6b756d6240774
Kevin Chaires <37939130+KevinChaires@users.noreply.github.com>
2023-03-03 16:18:12 -0600

    Add operations functions

---
+++ script.js
@@ -0,0 +1,20 @@
+let firstNumber = 1;
+let secondNumber =1;
+let result = 0;
+let operation = "";
+
+function add (firstNumber, secondNumber){
+    return firstNumber + secondNumber;
+}
+
+function substract (firstNumber, secondNumber){
+    return firstNumber - secondNumber;
+}
+
+function multiply (firstNumber, secondNumber){
+    return firstNumber * secondNumber;
+}
+
+function divide(firstNumber, secondNumber){
+    return firstNumber / secondNumber;
+}
