70afcb078253792467332b49b3dc5bf8271afa52
Kevin Chaires <37939130+KevinChaires@users.noreply.github.com>
2023-03-03 16:29:40 -0600

    Add operate function

--- script.js
+++ script.js
@@ -18,3 +18,30 @@ function multiply (firstNumber, secondNumber){
 function divide(firstNumber, secondNumber){
     return firstNumber / secondNumber;
 }
+
+function operate(operation, firstNumber, secondNumber){
+    let currentOperation = 0;
+    switch(operation){
+        case "add":
+            currentOperation += add(firstNumber, secondNumber);
+            break;
+        case "substract":
+            currentOperation += substract(firstNumber, secondNumber);
+            break;
+        case "multiply":
+            currentOperation += multiply(firstNumber, secondNumber);
+            break;
+        case "divide":
+            currentOperation += divide(firstNumber, secondNumber);
+            break;
+        default:
+            alert("Error");
+            break;
+    }
+    return currentOperation;
+}
+
+firstNumber = parseFloat(prompt("First number?"));
+operation = prompt("Add, substract multiply, or divide?");
+secondNumber = parseFloat(prompt("Second number?"));
+console.log(operate(operation, firstNumber, secondNumber));
