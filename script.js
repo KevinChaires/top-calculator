f81420a4cf2cfebf806feaf79f4399b449e5ad6e
Kevin Chaires <37939130+KevinChaires@users.noreply.github.com>
2023-03-03 21:18:41 -0600

    Add calculator functionality

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let firstNumber = 1;
-let secondNumber =1;
+let firstNumber = " ";
+let secondNumber = " ";
 let result = 0;
 let operation = "";
 
@@ -51,10 +51,116 @@ function operate(operation, firstNumber, secondNumber){
 let screenNumber = document.querySelector("#screenNumber");
 screenNumber.textContent = '0';
 
-//Test
+let actual = "";
+let currentNumber = "";
+let operationMode = "false";
+
+
 const buttons = document.querySelectorAll(".btn");
 for(const button of buttons){
     button.addEventListener('click', function(){
-        console.log(this.value);
-    });
-}
+        actual = this.value;
+        switch(actual){
+            case 'C':
+                console.log(actual);
+                firstNumber = " ";
+                secondNumber = " ";
+                result = 0;
+                operation = "";
+                actual = "";
+                currentNumber = "";
+                operationMode = "false";
+                screenNumber.textContent = "0";
+                break;
+            
+            case 'divide':
+                console.log(actual);
+                operationMode = "true";
+                operation = "divide";
+                if(firstNumber == " "){
+                    firstNumber = parseFloat(currentNumber);
+                }
+                else{
+                    secondNumber = parseFloat(currentNumber);
+                    result = operate(operation, firstNumber, secondNumber);
+                    screenNumber.textContent = result;
+                    firstNumber = result;
+                    secondNumber = " ";
+                }
+                break;
+
+            case 'multiply':
+                console.log(actual);
+                operationMode = "true";
+                operation = "multiply";
+                if(firstNumber == " "){
+                    firstNumber = parseFloat(currentNumber);
+                }
+                else{
+                    secondNumber = parseFloat(currentNumber);
+                    result = operate(operation, firstNumber, secondNumber);
+                    screenNumber.textContent = result;
+                    firstNumber = result;
+                    secondNumber = " ";
+                }
+                break;
+            
+            case 'substract':
+                console.log(actual);
+                operationMode = "true";
+                operation = "substract";
+                if(firstNumber == " "){
+                    firstNumber = parseFloat(currentNumber);
+                }
+                else{
+                    secondNumber = parseFloat(currentNumber);
+                    result = operate(operation, firstNumber, secondNumber);
+                    screenNumber.textContent = result;
+                    firstNumber = result;
+                    secondNumber = " ";
+                }
+                break;
+
+            case 'add':
+                operationMode = "true";
+                operation = "add";
+                console.log(actual);
+                if(firstNumber == " "){
+                    firstNumber = parseFloat(currentNumber);
+                    console.log("suma - firstNumber: " + firstNumber);
+                }
+                else{
+                    secondNumber = parseFloat(currentNumber);
+                    result = operate(operation, firstNumber, secondNumber);
+                    screenNumber.textContent = result;
+                    firstNumber = result;
+                    secondNumber = " ";
+                }
+                break;
+            
+            case 'equals':
+                console.log(actual);
+                secondNumber = parseFloat(currentNumber);
+                if(operation == ""){
+                    console.log("perame") //FALTA
+                }
+                else{
+                    result = operate(operation, firstNumber, secondNumber);
+                    screenNumber.textContent = result;
+                    firstNumber = result;
+                    secondNumber = " ";
+                }
+                break;
+
+            case 'Creator':
+                console.log(actual);
+                break;
+            default: //Numeros
+                console.log("Number " + actual);
+                operationMode == "true" ? currentNumber = actual : currentNumber += actual;
+                operationMode = "false";
+                console.log(currentNumber);
+                screenNumber.textContent = currentNumber;
+                break;
+        }
+    })};
